05770a94568ed74a1c492646fde5676180075655
ian038 <ianphua3@gmail.com>
2022-05-08 18:42:03 -0400

    Added functionality to register domains on chain

--- smart-contract/scripts/deploy.js
+++ smart-contract/scripts/deploy.js
@@ -1,12 +1,20 @@
 const hre = require("hardhat");
 
 async function main() {
+  const [owner, randomPerson] = await hre.ethers.getSigners();
   const Domains = await hre.ethers.getContractFactory("Domains");
   const domains = await Domains.deploy();
 
   await domains.deployed();
 
   console.log("Domains deployed to:", domains.address);
+  console.log("Domains deployed by:", owner.address);
+
+  const txn = await domains.register('abc')
+  await txn.wait()
+
+  const domainOwner = await domains.getAddress('abc')
+  console.log('Domain owner is:', domainOwner)
 }
 
 main()
