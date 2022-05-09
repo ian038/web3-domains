182540cb59c39d76f98a16a59edc6020cf9db487
ian038 <ianphua3@gmail.com>
2022-05-09 12:51:25 -0400

    Refactored deploy.js for Domains.sol

--- smart-contract/scripts/deploy.js
+++ smart-contract/scripts/deploy.js
@@ -3,18 +3,21 @@ const hre = require("hardhat");
 async function main() {
   const [owner, randomPerson] = await hre.ethers.getSigners();
   const Domains = await hre.ethers.getContractFactory("Domains");
-  const domains = await Domains.deploy();
+  const domains = await Domains.deploy('spartan');
 
   await domains.deployed();
 
   console.log("Domains deployed to:", domains.address);
   console.log("Domains deployed by:", owner.address);
 
-  let txn = await domains.register('abc')
+  let txn = await domains.register('abc', { value: hre.ethers.utils.parseEther('10.0') })
   await txn.wait()
 
   const domainOwner = await domains.getAddress('abc')
   console.log('Domain owner is:', domainOwner)
+
+  const balance = await hre.ethers.provider.getBalance(domains.address);
+  console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
 }
 
 main()
