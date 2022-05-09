9af51a97397603c10530d67226ea857e38de8ac5
ian038 <ianphua3@gmail.com>
2022-05-09 17:37:14 -0400

    Successfully minted domain

--- smart-contract/contracts/Domains.sol
+++ smart-contract/contracts/Domains.sol
@@ -35,7 +35,7 @@ contract Domains is ERC721URIStorage {
         uint256 len = StringUtils.strlen(name);
         require(len > 0);
         if (len == 3) {
-            return 5 * 10**17; // 5 MATIC = 5 000 000 000 000 000 000 (18 decimals). We're going with 0.5 Matic cause the faucets don't give a lot
+            return 2 * 10**17; // 2 MATIC = 2 000 000 000 000 000 000 (18 decimals).
         } else if (len == 4) {
             return 3 * 10**17; // To charge smaller amounts, reduce the decimals. This is 0.3
         } else {
--- smart-contract/scripts/deploy.js
+++ smart-contract/scripts/deploy.js
@@ -1,20 +1,22 @@
 const hre = require("hardhat");
 
 async function main() {
-  const [owner, randomPerson] = await hre.ethers.getSigners();
   const Domains = await hre.ethers.getContractFactory("Domains");
   const domains = await Domains.deploy('spartan');
 
   await domains.deployed();
-
   console.log("Domains deployed to:", domains.address);
-  console.log("Domains deployed by:", owner.address);
 
-  let txn = await domains.register('abc', { value: hre.ethers.utils.parseEther('10.0') })
+  let txn = await domains.register('silent', { value: hre.ethers.utils.parseEther('0.2') })
   await txn.wait()
+  console.log("Minted domain silent.spartan");
 
-  const domainOwner = await domains.getAddress('abc')
-  console.log('Domain owner is:', domainOwner)
+  txn = await domains.setRecord("silent", "https://ianphuadev.web.app/");
+  await txn.wait();
+  console.log("Set record for silent.spartan");
+
+  const address = await domains.getAddress("silent");
+  console.log("Owner of domain silent:", address);
 
   const balance = await hre.ethers.provider.getBalance(domains.address);
   console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
